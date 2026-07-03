1a2b972d92ffad711ef8f325bd82ae67c58e5db0
Tu Nombre <tu-correo@ejemplo.com>
2026-07-03 08:35:23 -0500

    Actualizacion galeria mujeres blog y videos

--- js/app.js
+++ js/app.js
@@ -330,4 +330,40 @@ lightbox.style.display="none";
 
 }
 
+});const lightbox =
+document.getElementById("lightbox");
+
+const lightboxImg =
+document.getElementById("lightbox-img");
+
+const closeLightbox =
+document.getElementById("close-lightbox");
+
+document
+.querySelectorAll(".gallery-grid img")
+.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+lightbox.style.display="flex";
+lightboxImg.src=img.src;
+
+});
+
+});
+
+closeLightbox.addEventListener("click",()=>{
+
+lightbox.style.display="none";
+
+});
+
+lightbox.addEventListener("click",(e)=>{
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
 });
